2d838ca8b0688a7a39cd4f24851c263161ea3cd9
Barisimbaris <barisakarkan@gmail.com>
2023-11-17 15:33:07 +0300

    Proje dosyası ekle.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+         {
+            Console.Write("Enter the number of cards to pick: ");
+            string line= Console.ReadLine();    
+           
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach(string card in CardPickers.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("please enter a valid number");
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+
+        }
+    }
+}
